81e8933cdd298211fd30491c556dbf9d986c1515
Fiononana-Jaofera <fiononana.andriatsalama@gmail.com>
2024-10-17 11:20:40 +0300

    update symbols

--- text/symbols.py
+++ text/symbols.py
@@ -4,13 +4,12 @@
 Defines the set of symbols used in text input to the model.
 """
 _pad = "_"
-_punctuation = ';:,.!?¡¿—…"«»“” '
-_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-_letters_ipa = "ɑɐɒæɓʙβɔɕçɗɖðʤəɘɚɛɜɝɞɟʄɡɠɢʛɦɧħɥʜɨɪʝɭɬɫɮʟɱɯɰŋɳɲɴøɵɸθœɶʘɹɺɾɻʀʁɽʂʃʈʧʉʊʋⱱʌɣɤʍχʎʏʑʐʒʔʡʕʢǀǁǂǃˈˌːˑʼʴʰʱʲʷˠˤ˞↓↑→↗↘'̩'ᵻ"
-
+_punctuation = ';:,.!?-"«»“”\' '
+_letters = "abdefghijklmnoôprstvyz"
+_letters_ipa = "d͡z#u#t͡s#ⁿt͡s#ⁿd͡z#ʈ͡ʂ#ɖ͡ʐ#ⁿʈ͡ʂ#ⁿɖ͡ʐ#ᵑk#ᵑɡ#ᵐp#ᵐb#ⁿt#ⁿd#ɵ"
 
 # Export all symbols:
-symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa)
+symbols = [_pad] + list(_punctuation) + list(_letters) + _letters_ipa.split("#")
 
 # Special symbol ids
 SPACE_ID = symbols.index(" ")
